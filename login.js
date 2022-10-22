a159c8ddd017d9602c7cc627b7fa400aa347026d
USERNAME <firdauskun00@gmail.com>
2022-10-22 18:53:37 +0800

    logic login

---
+++ login.js
@@ -0,0 +1,32 @@
+// login
+// document.getElementById('btn-login').addEventListener('click',function(event){
+//     let emailLogin = document.getElementById('email-login').value
+//     let passLogin = document.getElementById('password-login').value 
+//     event.preventDefault()
+// })
+
+async function checkAkun(email,password){
+    let data = await fetch('https://63528ae6a9f3f34c37409536.mockapi.io/logres');
+
+    let dataAkun = await data.json();
+    let hasil = false;
+    
+    dataAkun.forEach(element => {
+        if(element.email == email && element.password == password){
+            hasil = true;
+        }
+    });
+
+    if(hasil){
+        console.log("berhasil login");
+    }else{
+        console.log("email/password salah!");
+    }
+    // console.log(dataAkun)
+}
+
+document.querySelector("button").addEventListener("click", async function(e){
+    e.preventDefault();
+    checkAkun(document.getElementById("email-login").value,document.getElementById("password-login").value)
+})
+// checkAkun()  
